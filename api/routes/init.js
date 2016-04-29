d58766221decaae350a9626893f3e94c2188b572
KIMbABERer <admin@KIMSTER.local>
2016-04-29 14:24:09 -0400

    sd

--- api/routes/init.js
+++ api/routes/init.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get('/',function(req,res){
 	//check if admin user exists
-	var where = {where:{email:'admin@shop.com'}};
+	var where = {where:{email:'boss@spartan.com'}};
 	models.Users.findAll(where).then(function(users){
 		if('0' in users){
 			res.send('Admin Account Already Exists');
@@ -12,8 +12,8 @@ router.get('/',function(req,res){
 		else{
 			//admin user obj
 			var user_obj = {
-				email:'admin@shop.com',
-				password:'brainstation'
+				email:'boss@spartan.com',
+				password:'spartan'
 			}
 			//add to database model and respond with object
 			models.Users.create(user_obj).then(function(users){
@@ -21,7 +21,7 @@ router.get('/',function(req,res){
 					users:users
 				});
 			});
-			res.send('<h1>Admin Account Created</h1><p>Log In:admin@shop.com<br>Password:brainstation</p>');
+			res.send('<h1>Admin Account Created</h1><p>Log In:boss@spartan.com<br>Password:spartan</p>');
 		}
 		
 	});
