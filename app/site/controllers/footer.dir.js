5446150fa409074dead40267ded4dc5e6dfb04e9
ryanchick <uw.ryanchick@gmail.com>
2016-04-29 00:11:48 -0400

    tons of changes

--- app/site/controllers/footer.dir.js
+++ app/site/controllers/footer.dir.js
@@ -9,15 +9,28 @@
         templateUrl: "/site/partials/footer.html"
 
       };
-});
+  });
 
-angular
-  .module("shopApp")
-  .controller("footerCtrl", footerCtrl);
+  angular
+    .module("shopApp")
+    .controller("footerCtrl", footerCtrl);
 
 
-function footerCtrl($scope, $state){
+  function footerCtrl($scope, $state,$location){
+    console.log('footer')
+    this.is_admin = false;
+    // console.log($state.current.name)
+    // console.log($location)
+    if($state.current.name.search('admin') != -1){
+      this.is_admin=true;
+    }
 
-}
-
+    $scope.$watch($state.current.name,function(){
+      // console.log($state.current.name)
+      if($state.current.name.search('admin') != -1){
+        this.is_admin=true;
+      }
+    })
+    console.log(this.is_admin)
+  }
 })();
